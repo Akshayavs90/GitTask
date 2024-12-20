b8073987a80f19db46cb3d9017edccc3b4525e73
V S <akshaya.v-s@capgemini.com>
2024-12-20 17:14:38 +0530

    first commit

---
+++ src/GITTASK/GitDec.java
@@ -0,0 +1,10 @@
+package GITTASK;
+
+public class GitDec {
+
+	public static void main(String[] args) {
+		System.out.println("Msg from STS");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author akshvs
+ *
+ */
+module GitHubTask {
+}
